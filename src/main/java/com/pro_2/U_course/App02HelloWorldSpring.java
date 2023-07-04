ff59eeb3e6a3bf80dc89e906e672ddfa85871e2c
Anush Serrao <60592571+ashserrao@users.noreply.github.com>
2023-07-04 22:06:01 +0530

    class 19 section 1

--- src/main/java/com/pro_2/U_course/App02HelloWorldSpring.java
+++ src/main/java/com/pro_2/U_course/App02HelloWorldSpring.java
@@ -2,14 +2,19 @@ package com.pro_2.U_course;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class App02HelloWorldSpring {
     public static void main(String[] args) {
         //Launch spring context
         var context = new AnnotationConfigApplicationContext(HelloWorldConfiguration.class);
-        System.out.println("My name is " + context.getBean("name"));
-        System.out.println("I'm " + context.getBean("age"));
-        System.out.println(context.getBean("person"));
-        System.out.println(context.getBean("address"));
+//        System.out.println("My name is " + context.getBean("name"));
+//        System.out.println("I'm " + context.getBean("age"));
+//        System.out.println(context.getBean("person"));
+//        System.out.println(context.getBean("astro"));
+//        System.out.println(context.getBean("personMethodCall"));
+        Arrays.stream(context.getBeanDefinitionNames()).forEach(System.out::println);
 
     }
 }
--- src/main/java/com/pro_2/U_course/HelloWorldConfiguration.java
+++ src/main/java/com/pro_2/U_course/HelloWorldConfiguration.java
@@ -4,13 +4,17 @@ import org.springframework.boot.autoconfigure.amqp.RabbitConnectionDetails;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-record Person (String name, int age){ };
+record Person (String name, int age, Address address){ };
 record Address (String firstLine, String city){ };
 @Configuration
 public class HelloWorldConfiguration {
     // you can define spring beans
     // define the method of creating spring beans
 
+    @Bean
+    public Person personMethodCall() {
+        return new Person(name(), age(), address3());
+    }
     @Bean
     public String name(){
         return "Anush";
@@ -21,9 +25,17 @@ public class HelloWorldConfiguration {
     }
     @Bean
     public Person person() {
-        return new Person("Sachin", 22);
+        return new Person("Sachin", 22, new Address("1st Line", "Blr"));
     }
     @Bean
+     public Person personparameter( String name, int age, Address astro){
+        return new Person(name,age,astro);
+    }
+    @Bean
+    public Address address3() {
+        return new Address("Huskur gate", "bangalore");
+    }
+    @Bean (name = "astro")
     public Address address() {
         return new Address("First Line", "Blr");
     }
